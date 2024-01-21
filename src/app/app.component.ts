984e9b4a5d1a09dda5553e5547f2e478431ac590
sebastins18 <sebastins08@gmail.com>
2024-01-20 22:35:48 -0600

    Implemented a 'Scroll to Top' button enabling users to automatically scroll back to the top of the page. This button appears only when the user scrolls down and hides when at the top of the page. Adjustments in the HTML structure of the alls section to improve accessibility and user experience. General optimizations in CSS to enhance visual consistency and aesthetics of the all sections.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
+import { Router, NavigationEnd, Event as RouterEvent } from '@angular/router';
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,27 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'astro_pagina_info';
+  mostrarSeccionesAnclaje = true;
+
+  constructor(private router: Router) {
+    this.router.events.pipe(
+      filter((event: RouterEvent): event is NavigationEnd => event instanceof NavigationEnd)
+    ).subscribe((event: NavigationEnd) => {
+      this.mostrarSeccionesAnclaje = !event.url.includes('/faqs');
+    });
+  }
+
+  mostrarBotonIrArriba = false;
+
+  @HostListener('window:scroll', ['$event'])
+  onWindowScroll() {
+    // Establece un umbral de desplazamiento, por ejemplo, 100px
+    const umbral = 100;
+    const yOffSet = window.pageYOffset;
+    this.mostrarBotonIrArriba = yOffSet > umbral;
+  }
+
+  scrollToTop() {
+    window.scroll({ top: 0, left: 0, behavior: 'smooth' });
+  }
 }
